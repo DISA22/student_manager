8a8d6a3d783aa4131e73b85da50faf3372bf8bb2
DISA22 <dinar2.sharafutdinov@yandex.ru>
2025-11-27 19:32:08 +0500

    Перекинули на мавен

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import domain.Schedule;
+import repository.PasswordRepository;
+import repository.StudentScheduleRepository;
+import service.AuthorizationService;
+import service.RegistrationService;
+import service.ScheduleAndGradeService;
+import ui.ConsoleUI;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        PasswordRepository passwordRepository = new PasswordRepository();
+        StudentScheduleRepository studentScheduleRepository = new StudentScheduleRepository();
+        RegistrationService registrationService = new RegistrationService(passwordRepository, studentScheduleRepository);
+        Schedule schedule = new Schedule();
+        AuthorizationService authorizationService = new AuthorizationService(passwordRepository);
+        ScheduleAndGradeService scheduleAndGradeService = new ScheduleAndGradeService(studentScheduleRepository, schedule);
+        ConsoleUI consoleUI = new ConsoleUI(registrationService, authorizationService, scheduleAndGradeService);
+        consoleUI.start();
+    }
+}
